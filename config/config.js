20f28497103725cf2c99b92681103b7a51259380
Siebe Bosch <siebe@hydroconsult.nl>
2023-09-25 09:18:03 +0200

    'gitignore'

---
+++ config/config.js
@@ -0,0 +1,7 @@
+export const MAPBOX_CONFIG = {
+    MAPBOX_TOKEN: 'pk.eyJ1Ijoic2llYmVib3NjaCIsImEiOiJjazh1NnBqdDIwMW92M3FrZDF6YmNndDMyIn0.7J3-b577pBYCg1XWhmejXA'
+};
+
+export const FA_CONFIG = {
+    FONTAWESOME_TOKEN: '42e5b77852'
+};
