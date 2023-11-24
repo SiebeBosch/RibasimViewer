2341ac51ac0d5a52f3e7a327a57840e0eab39de0
Siebe Bosch <siebe@hydroconsult.nl>
2023-11-24 20:36:45 +0100

    'polygon_layer'

---
+++ webviewer_html/js/areas.js
@@ -0,0 +1,12 @@
+let Areas =   {
+        "type": "FeatureCollection",
+        "name": "areas",
+		"extent": {
+		    "minLat":51.6635399529209,
+		    "minLng":5.63627988780709,
+		    "maxLat":51.7358304780482,
+		    "maxLng":5.75738698725062
+		},
+        "features": [
+            ]
+        }
